062389bb04620ec5e2678331813c1f7a16c4a319
ZhangHao <zhanghao@antigen.top>
2024-03-11 14:43:17 +0800

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "note")
-(define racket-launcher-names (list "display-note"))
-(define racket-launcher-libraries (list "main.rkt"))
+(define racket-launcher-names (list "display-note" "update-note"))
+(define racket-launcher-libraries (list "main.rkt" "update.rkt"))
 (define install-collection "installer.rkt")
 (define deps (list "base"
                    "web-server-lib"
